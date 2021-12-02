b649876f385ef68e43c85109f7532af2442ff6e1
Alex Resiga <resiga.alex@gmail.com>
2021-12-02 17:36:37 +0200

    day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,4 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+fun main() = advent<Int> { input ->
+    input.windowed(2).count { (a, b) -> a < b }.print()
+    input.windowed(3) { it.sum() }.windowed(2).count { (a, b) -> a < b }.print()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,9 +5,21 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput() = File("src", "Input.txt").readLines()
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+
+inline fun <reified T> advent(block: (input: List<T>) -> Any) {
+    val input = when (T::class) {
+        Int::class -> readInput().map { it.toInt() } as List<T>
+        String::class -> readInput() as List<T>
+        else -> emptyList()
+    }
+    block.invoke(input)
+}
+
+fun Any.print() = println(this)
